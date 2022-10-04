0e3b64b71bb721511501fed3d8db9b63495b1b17
nathan <nathanmark306@gmail.com>
2022-10-04 14:14:12 +0100

    fine turning

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi I'm Nathan a software Engineer and Senior Man in the making</p>
+        <p>Hi I'm Nathan a software Engineer</p>
         <p>
           (This is a sample website - you’ll be building a site like this on
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
